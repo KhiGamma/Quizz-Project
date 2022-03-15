4341558e118d72256623958d2d0d0891f85e22e0
ArnaudJuilien <61822174+ArnaudJuilien@users.noreply.github.com>
2022-03-15 16:26:13 +0100

    microservice quizz created

--- quizz/src/main/java/fr/insa/quizz/QuizzApplication.java
+++ quizz/src/main/java/fr/insa/quizz/QuizzApplication.java
@@ -2,7 +2,9 @@ package fr.insa.quizz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class QuizzApplication {
 
